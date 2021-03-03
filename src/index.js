c8fca3918202fb07b6cd2885e7a923654a25526d
megu3k <49593780+megu3k@users.noreply.github.com>
2021-03-03 23:33:12 +0900

    declaration of variables const,letなどの変数宣言

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import "./styles.css";
+// /**
+//  * const,let変数宣言
+//  */
+// var val1 = "var変数宣言";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+//  val1 = "var変数を上書き"
+//  console.log(val1);
+
+//  // var変数は再宣言可能
+//  var val1 = "var変数を再宣言"
+//  console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "letは上書き可能"
+// console.log(val2);
+
+// // letは再宣言不可
+
+const val3 = "const変数";
+console.log(val3);
+
+//　const変数は上書き不可能
+
+// constで定義したオブジェクトのプロパティは変更が可能
+// const val4 = {
+//   name: "meg",
+//   age:0,
+// };
+// val4.name="megu";
+// val4.addres ="yokohama";
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
