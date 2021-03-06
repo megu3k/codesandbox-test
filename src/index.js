e3a2eda83d03ff4ef9c7a182442d305466376534
megu3k <49593780+megu3k@users.noreply.github.com>
2021-03-06 14:35:31 +0900

    destructuring assignment

--- src/index.js
+++ src/index.js
@@ -54,24 +54,47 @@
 //const message2 = `私の名前は${name}です。年齢は${age}です。`;
 //console.log(message2);
 
-/**
- *  アロー関数
- */
-// 従来の関数
-function func1(str) {
-  return str;
-}
-console.log(func1("func1です。"));
+// /**
+//  *  アロー関数
+//  */
+// // 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("func1です。"));
 
-// アロー関数
-const func2 = (str) => {
-  return str;
-};
-// ※直接returnのかわりに=>strができる。
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// // ※直接returnのかわりに=>strができる。
 
-console.log(func2("func2です。"));
+// console.log(func2("func2です。"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+// /**
+//  * 分割代入
+//  */
+// const myProfile = {
+//   name: "meg",
+//   age: 20
+// };
+
+//const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+//console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ['meg', 20];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name , age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`
+// console.log(message4);
